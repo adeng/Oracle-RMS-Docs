43145cf034376e13003d2a0961c5cf64465d73dc
Albert Deng <adeng@media.ucla.edu>
2016-12-10 16:28:58 -0800

    Changed serialization

--- js/module/app.js
+++ js/module/app.js
@@ -1,5 +1,44 @@
 (function() {
-	var home = angular.module('main', ['ui.bootstrap', 'ui.router', 'ngFileUpload', 'main.controllers', 'main.services'])
+	var home = angular.module('main', ['ui.bootstrap', 'ui.router', 'ngFileUpload', 'main.controllers', 'main.services'], function($httpProvider) {
+  	// Use x-www-form-urlencoded Content-Type
+  	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+	var param = function(obj) {
+	var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+	for(name in obj) {
+		value = obj[name];
+
+		if(value instanceof Array) {
+		for(i=0; i<value.length; ++i) {
+			subValue = value[i];
+			fullSubName = name + '[' + i + ']';
+			innerObj = {};
+			innerObj[fullSubName] = subValue;
+			query += param(innerObj) + '&';
+		}
+		}
+		else if(value instanceof Object) {
+		for(subName in value) {
+			subValue = value[subName];
+			fullSubName = name + '[' + subName + ']';
+			innerObj = {};
+			innerObj[fullSubName] = subValue;
+			query += param(innerObj) + '&';
+		}
+		}
+		else if(value !== undefined && value !== null)
+		query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+	}
+
+	return query.length ? query.substr(0, query.length - 1) : query;
+	};
+
+	// Override $http service's default transformRequest
+	$httpProvider.defaults.transformRequest = [function(data) {
+		return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+	}];
+	})
 	
 	.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 		$stateProvider
